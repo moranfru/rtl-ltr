47657598be4b294fc027a52fb288cce07f67de3d
moranfru <moranf@wix.com>
2026-01-01 12:20:30 +0200

    version v8

--- rtl-ltr.js
+++ rtl-ltr.js
@@ -1,6 +1,6 @@
 
 (function() {
-  console.log('rtl-ltr.js v7');
+  console.log('rtl-ltr.js v8');
   // --- CONFIGURATION ---
   const RTL_LANGS = ['he'];
   const TARGET_PREFIXES = ['wixui-', 'StylableHorizontalMenu'];
